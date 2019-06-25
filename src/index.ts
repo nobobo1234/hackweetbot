ddc324bd1d4eebe64139bdc8357bebc0fe75f588
Nathan Graule <solarliner@gmail.com>
2019-06-26 00:49:40 +0200

    Persist training to disk and add heuristic to detect garbage output

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,76 @@
 import { config } from "dotenv";
+import { Client, TextChannel, Message } from "discord.js";
+import { createProcessor, loadClassifier, saveClassifier, createClassifier } from "./nlp";
+import { stat, exists, mkdir } from "fs";
+import { join, dirname } from "path";
+import { BayesClassifier } from "natural";
+import { promisify } from "util";
+
+const existsP = promisify(exists);
+const mkdirP = promisify(mkdir);
+const statP = promisify(stat);
 
 config();
+main().catch(err => {
+  if (err instanceof Error) console.error(err.name, err.message, err.stack);
+  else console.error(err);
+  process.exit(1);
+});
 
-function main() {
-  console.log("Hello world!");
+async function main() {
+  const client = new Client();
+  const classifier = await createOrLoadClassifier();
+  const processor = createProcessor(classifier, {
+    reminder: input => `Reminder command called: ${input}`,
+    todo: input => `Todo command called: ${input}`
+  });
+  client.on("ready", () => console.log("Discord bot is ready!"));
+  client.on("message", m => processMessage(client, processor, m));
+
+  client.login(process.env.DISCORD_BOT_TOKEN);
 }
 
-main();
+function processMessage(client: Client, processor: ReturnType<typeof createProcessor>, message: Message): void {
+  console.log(
+    new Date(),
+    message.channel instanceof TextChannel ? message.channel.name : message.channel.id,
+    message.content
+  );
+  // Don't process bot messages, or messages not target at the bot
+  if (message.author.bot) return;
+  if (!message.mentions.users.has(client.user.id)) return;
+  try {
+    const res = processor(message);
+    if (typeof res === "string") {
+      message.channel.send(res, { reply: message.author });
+    } else {
+      const [msg, opts] = res;
+      message.channel.send(msg, opts);
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(new Date(), err.name, err.message);
+      message.channel.send(`:x: Error! [${err.name}] ${err.message}`);
+    } else {
+      console.error(new Date(), err);
+      message.channel.send(`:x: Error! ${err}`);
+    }
+  }
+}
+
+async function createOrLoadClassifier(): Promise<BayesClassifier> {
+  const trainFilePath = join(process.cwd(), "data", "classifier.json");
+  if (await existsP(trainFilePath)) {
+    return loadClassifier(trainFilePath);
+  } else {
+    const c = await createClassifier();
+    const trainFileDir = dirname(trainFilePath);
+    const e = await existsP(trainFileDir);
+    if (!e) await mkdirP(trainFileDir, { recursive: true });
+
+    if (!(await statP(trainFileDir)).isDirectory())
+      throw new Error("Can't write file into non-folder at " + dirname(trainFilePath));
+    await saveClassifier(trainFilePath, c);
+    return c;
+  }
+}
--- src/nlp.ts
+++ src/nlp.ts
@@ -1,21 +1,52 @@
 import { PorterStemmer, BayesClassifier } from "natural";
+import { Message, MessageOptions } from "discord.js";
+import { writeFile, readFile } from "fs";
+import { promisify } from "util";
+import { join } from "path";
 
-type InputFunction<T> = (input: string) => T;
+type InputFunction = (input: string, msg: Message) => string;
+type MessageResult = string | [string, MessageOptions];
 
-export function buildClassifier() {
+export function createProcessor(
+  classifier: BayesClassifier,
+  commands: Record<"reminder" | "todo", InputFunction>
+): (message: Message) => MessageResult {
+  return processInput.bind(null, classifier, commands);
+}
+
+export function saveClassifier(path: string, classifier: BayesClassifier): Promise<void> {
+  return new Promise((resolve, reject) => {
+    writeFile(path, JSON.stringify(classifier), err => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
+}
+export function loadClassifier(path: string): Promise<BayesClassifier> {
+  return new Promise((resolve, reject) => {
+    readFile(path, (err, data) => {
+      if (err) reject(err);
+      resolve(BayesClassifier.restore(JSON.parse(data.toString())));
+    });
+  });
+}
+
+export async function createClassifier() {
   const classifier = new BayesClassifier();
-  classifier.addDocument(["remind", "remind me", "tell me"], "reminder");
-  classifier.addDocument(["todo", "list"], "todo");
-
+  const trainFilepath = join(process.cwd(), "data", "trainData.json");
+  const trainData: CommandTrainingData = JSON.parse(await promisify(readFile)(trainFilepath).then(d => d.toString()));
+  for (const key of Object.keys(trainData)) {
+    classifier.addDocument(trainData[key], key);
+  }
   classifier.train();
   return classifier;
 }
 
-export function buildCommandsObjject() {
-  const commands: Record<string, InputFunction<any>> = {};
+export function buildCommandsObject() {
+  const commands: Record<string, InputFunction> = {};
 
   return {
-    addCommand<T>(name: string, handler: InputFunction<T>) {
+    addCommand<T>(name: string, handler: InputFunction) {
       commands[name] = handler;
       return this;
     },
@@ -25,15 +56,19 @@ export function buildCommandsObjject() {
   };
 }
 
-export function processInput<T>(
-  classifier: BayesClassifier,
-  commands: Record<string, InputFunction<T>>,
-  input: string
-): T {
-  const key = classifier.classify(input);
-  if (key in commands) {
-    return commands[key](input);
-  } else {
+function processInput(classifier: BayesClassifier, commands: CommandHandlers, message: Message): MessageResult {
+  const input = message.cleanContent;
+  const outputs = classifier.getClassifications(input).sort((a, b) => b.value - a.value);
+  const first = outputs[0];
+  const second = outputs[1];
+  if (first.value - second.value < 0.1) return [`Sorry, could not recognize a command\n${JSON.stringify(outputs)}`, {}];
+  const key = outputs[0].label;
+  if (!(key in commands)) {
     throw new Error(`Command "${key}" does not exists`);
   }
+  const result = commands[key](input, message);
+  return [result + `\n${JSON.stringify(outputs)}`, { reply: message.author }];
 }
+
+type CommandTrainingData<C extends string = string> = Record<C, string[]>;
+type CommandHandlers<C extends string = string> = Record<C, InputFunction>;
